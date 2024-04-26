8005a918ed3be2c6c219242fc475cff520f3aea9
Lianna Maley <liannamaley@wifi-64-187-241-206.nyit.edu>
2024-04-26 18:46:21 -0400

    gonow

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Title from './components/title';
+import Title from './components/title.jsx';
+import Data from './data/data.js'
 import MediaCard from './components/employeeCard.jsx';
-import Data from './data/data.js';
 import Container from '@mui/material/Container';
-
-
+import Button from '@mui/material/Button';
 function App() {
-  const [count, setCount] = useState(0)
-console.log(Data);
-  return (
-    <Container fixed>
-    <MediaCard />
-    </Container>
-  )
+ const [onLeave, setLeave] = useState(true);
+console.log(onLeave);
+ return (
+   <Container fixed>
+<Button variant="text">Text</Button>
+     <Button variant="contained">Contained</Button>
+     <Button variant="outlined">Outlined</Button>
+   <MediaCard data={Data} />
+   </Container>
+
+ )
 }
 
 export default App
--- src/components/employeeCard.jsx
+++ src/components/employeeCard.jsx
@@ -3,45 +3,62 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import PetsIcon from '@mui/icons-material/Pets';
 import Box from '@mui/material/Box';
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
+import { ListItemAvatar } from '@mui/material';
 
 
-export default function MediaCard() {
-  return (
-    <Card sx={{ maxWidth: 345, padding:2 }}>
-      <Box sx={{paddingTop: 2 }}> 
-      <CardMedia
-        sx={{ height: 300, width: 300, backgroundSize: "contain" ,margin: "auto" }}
-        image="/employee-dashboard-react-app/profile1.png"
-        title="profile image"
-      />
-      </Box>
-      <EmojiNatureIcon/>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          John Smith
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-         Department: UX/UI Design
-        </Typography>
-        <Typography>
-          Position: UI Design
-        </Typography>
-        <Stack direction="row" spacing={1} sx={{ marginTop: 2, justifyContent: "center" }}>
-      <Chip label="figma" />
-      <Chip label="Adobe XD" />
-      <Chip label="Sketch" />
-    </Stack>
-      </CardContent>
-      <Stack direction="row" spacing={1} sx={{ marginTop: 2, justifyContent: "center" }}>
-      <Chip label="Active" variant="outlined"/>
-      <Chip label="Hours: 20" variant="outlined" />
-    </Stack>
-    </Card>
-  );
-}
+export default function MediaCard({ data }) {
+ /* console.log(data); */
+ return (
+   <>
+   {
+     data.map((item, index) => (
+<Card sx={{ maxWidth: 345, padding: 2, marginTop:2 }}>
+     <Box >
+     <CardMedia
+       sx={{
+          height: 240, width: 300, backgroundSize: "contain",margin: "auto"}}
+       image={`/employee-dashboard-react-app/${item.image}`}
+       title="profile image"
+     />
+     </Box>
+     <PetsIcon />
+     <CardContent>
+       <Typography gutterBottom variant="h5" component="div">
+      {item.firstName} {item.lastName}
+       </Typography>
+       <Typography variant="body2" color="text.secondary">
+        {item.position}
+       </Typography>
+       <Stack direction="row"
+       flexWrap="wrap"
+       spacing={1} sx={{marginTop: 2, justifyContent: "center" }}>
+     {item.skills.map((skill, skillIndex) => (
+       <Chip sx={{marginTop: 2 }}key={skillIndex} label={skill} />
+
+     ))}
+   </Stack>
+
+
+     </CardContent>
+     <Stack direction="row" spacing={1}  sx={{marginTop: 2, justifyContent: "center" }}>
+     <Chip label={item.onLeave? "Inacive" : "Active"}
+     variant={item.onleave ? "outlined" : ""} />
+     <Chip 
+     label={`Hours: ${item.hoursLoggedThisWeek}`}
+      variant="outlined"
+       />
+   </Stack>
+
+   </Card>
+
+   ))}
+</>
+   
+ );
+} 
