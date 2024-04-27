0d3162a9ac31837e0253c996acf7c3e5327b7b0a
Lianna Maley <liannamaley@wifi-64-187-241-206.nyit.edu>
2024-04-26 20:43:19 -0400

    yassss

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,33 @@ import Data from './data/data.js'
 import MediaCard from './components/employeeCard.jsx';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import { Stack } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
+
 function App() {
- const [onLeave, setLeave] = useState(true);
-console.log(onLeave);
+ const [ShowOnLeave, setShowOnLeave] = useState(false);
+console.log(ShowOnLeave);
+
+const filteredData = ShowOnLeave ? Data.filter((employee) => employee.onLeave): Data;
+
+const toggleOnLeave = (event) => { if(ShowOnLeave) {setShowOnLeave(false)} else setShowOnLeave(true)};
+
  return (
    <Container fixed>
-<Button variant="text">Text</Button>
-     <Button variant="contained">Contained</Button>
-     <Button variant="outlined">Outlined</Button>
-   <MediaCard data={Data} />
+    <Box>
+    <Typography variant="h4" component="h1">
+        Employee Dashboard 
+      </Typography>
+      </Box>
+      <Stack spacing={2} direction="row" justifyContent="center"> 
+      <Switch onChange={toggleOnLeave}/>
+      </Stack>
+   <MediaCard data={filteredData} />
    </Container>
 
- )
+ );
 }
 
 export default App
